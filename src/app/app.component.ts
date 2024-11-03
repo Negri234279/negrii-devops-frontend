d520198498f1b097fa0befbafe23d50ada2c5558
Negri234279 <rafalejo01@gmail.com>
2024-11-03 21:12:41 +0100

    feat mejorar la estructura de la aplicación; agregar encabezado, navegación y animaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { Component, inject, OnInit } from '@angular/core'
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
+import { filter } from 'rxjs'
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
     templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    private readonly router = inject(Router)
+
+    isNotFound = false
+
+    headerLinks: HeaderLink[] = [
+        { url: '/characters', text: 'Characters' },
+        { url: '/locations', text: 'Locations' },
+        { url: '/episodes', text: 'Episodes' },
+    ]
+
+    ngOnInit(): void {
+        this.router.events.pipe(filter((ev) => ev instanceof NavigationEnd)).subscribe((ev) => {
+            this.isNotFound = ev.url === '/not-found' || ev.urlAfterRedirects === '/not-found'
+        })
+    }
+}
+
+interface HeaderLink {
+    url: string
+    text: string
+}
