d7ebc926cd1867be3c4cc6cb2238fd584f605501
Negri234279 <rafalejo01@gmail.com>
2024-11-03 21:12:07 +0100

    feat add not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router'
+import { CharactersComponent } from './pages/characters/characters.component'
+import { NotFoundComponent } from './pages/not-found/not-found.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+    {
+        path: 'characters',
+        component: CharactersComponent,
+    },
+    {
+        path: 'not-found',
+        component: NotFoundComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '/not-found',
+    },
+]
