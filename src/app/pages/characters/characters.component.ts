0b26e8b4c3e86ac5f2190e5910bd22a5a6221012
Negri234279 <rafalejo01@gmail.com>
2024-11-03 21:12:20 +0100

    feat agregar componente de personajes con pruebas y plantilla

---
+++ src/app/pages/characters/characters.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-characters',
+    standalone: true,
+    imports: [],
+    templateUrl: './characters.component.html',
+})
+export class CharactersComponent {}
