545884875d95f5077eb2af42ee779057f81ebbbf
Negri234279 <rafalejo01@gmail.com>
2024-11-04 19:28:47 +0100

    feat agregar componente de inicio; implementar la estructura y el diseño de la página de inicio con enlaces a secciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router'
-import { CharactersComponent } from './pages/characters/characters.component'
+
+import { CharactersComponent } from './pages/characters/characters/characters.component'
+import { HomeComponent } from './pages/home/home.component'
 import { NotFoundComponent } from './pages/not-found/not-found.component'
 
 export const routes: Routes = [
     {
+        path: '',
+        component: HomeComponent,
+    },
+    {
         path: 'characters',
         component: CharactersComponent,
+        data: {
+            title: 'Characters',
+        },
     },
     {
         path: 'not-found',
