0c8b068b80c614340f0e9a57f6a32402d012aae0
Negri234279 <rafalejo01@gmail.com>
2024-11-04 23:04:34 +0100

    feat actualizar configuración del enrutador; agregar soporte para binding de entrada de componentes y transiciones de vista

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { provideHttpClient, withFetch } from '@angular/common/http'
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 import { provideClientHydration } from '@angular/platform-browser'
-import { provideRouter } from '@angular/router'
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router'
 
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
+        provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
         provideClientHydration(),
         provideHttpClient(withFetch()),
     ],
